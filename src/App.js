f6c2cc949e6ff3a178506c0cd25dee8b8472a5e1
Arnab Das <adas@aapnainfotech.com>
2021-12-28 18:25:28 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import HomePage from './homepage.component';
 import './homepage.styles.scss'
 function App() {
-  return  <div><HomePage/></div>
+  return <div><HomePage/></div>
 }
 
 export default App;
