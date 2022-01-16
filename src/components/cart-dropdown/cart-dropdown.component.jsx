4d0931df356a0531599c53738a28f950b1f46791
Arnab Das <adas@aapnainfotech.com>
2022-01-16 20:58:16 +0530

    added cart-icon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,24 @@
-import '../cart-dropdown/cart-dropdown.styles.scss'
-import CustomButton from '../custom-button/custom-button.component'
+import React from 'react';
+import { connect } from 'react-redux';
 
-const CartDropdown = ()=>(
-<div className='cart-dropdown'>
-<div className='cart-items'>
-</div>
-<CustomButton>GO TO CHECKOUT</CustomButton>
-</div>
-)
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-export default CartDropdown
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = ({ cartItems }) => (
+  <div className='cart-dropdown'>
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
+    <CustomButton>GO TO CHECKOUT</CustomButton>
+  </div>
+);
+
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
